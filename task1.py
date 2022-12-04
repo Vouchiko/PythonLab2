c5853f2ab02dfcdc8075ed98dd645c1d301a3ac1
vouchiko <vovakarpov2017@gmail.com>
2022-12-04 18:14:12 +0400

    Added docstring

--- task1.py
+++ task1.py
@@ -5,6 +5,7 @@ import csv
 def create_annotation(class_name: str, annotation_name: str) -> None:
     """This function creates a cvs file with three parameters: the absolute path to the file, the relative path to the
     file and the class label """
+
     path_class = os.path.join('dataset', class_name)
     class_names = os.listdir(path_class)
     with open(annotation_name, mode="w", encoding="UTF-16", newline='') as file:
