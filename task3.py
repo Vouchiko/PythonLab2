2770955a71af0f768877b81a2b8f251d712218a2
vouchiko <vovakarpov2017@gmail.com>
2022-12-04 18:14:30 +0400

    Added docstring

--- task3.py
+++ task3.py
@@ -6,17 +6,21 @@ from email import generator
 
 
 def create_dir(dir_name: str) -> str:
+    """This function create directory "dataset2" where we must copy our dataset"""
     if not os.path.isdir(dir_name):
         os.mkdir(dir_name)
     return dir_name
 
 
 def get_element(class_name: str) -> generator:
+    """this function returns us a list of names in the dataset class"""
     for file_name in os.listdir(os.path.join("dataset", class_name)):
         yield file_name
 
 
 def create_randname_file(annotation_name: str, dir_copy: str) -> None:
+    """This function creates a copy of the dataset so that each file from the source dataset receives a random number
+    from 0 to 10000, and the dataset is the following structure dataset/number.jpg. """
     file_number = list(range(10001))
     random.shuffle(file_number)
     count = 1
