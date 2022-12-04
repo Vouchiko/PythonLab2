c50a0ef72cda120635f12388dc15bb01edeecb94
vouchiko <vovakarpov2017@gmail.com>
2022-12-04 17:23:00 +0400

    Added the third iterator class

--- task5.py
+++ task5.py
@@ -1,5 +1,5 @@
 import os
-
+import csv
 
 class IteratorTask1:
     def __init__(self, path: str):
@@ -19,6 +19,30 @@ class IteratorTask1:
 class IteratorTask2:
     def __init__(self, class_name: str, path: str):
         self.file_names = os.listdir(os.path.join(path))
+        for name in self.file_names:
+            if not class_name in name:
+                self.file_names.remove(name)
+
+        self.limit = len(self.file_names)
+        self.counter = 0
+        self.path = path
+
+    def __next__(self):
+        if self.counter < self.limit:
+            self.counter += 1
+            return os.path.join(self.path, self.file_names[self.counter - 1])
+        else:
+            raise StopIteration
+
+
+class IteratorTask3:
+    def __init__(self, class_name: str, path: str, annotation_name: str):
+        self.file_names = list()
+        with open(os.path.join(path, annotation_name), encoding='UTF-16') as file:
+            reader = csv.reader(file, delimiter=',')
+            for file_info in reader:
+                if file_info[1] == class_name:
+                    self.file_names.append()
         for name in self.file_names:
             if not class_name in name:
                 self.file_names.remove(name)
