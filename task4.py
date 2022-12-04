893109fcb9e2217c232a4d01ac357248e96efbb3
vouchiko <vovakarpov2017@gmail.com>
2022-12-04 18:15:41 +0400

    Added docstring

--- task4.py
+++ task4.py
@@ -3,6 +3,7 @@ import os
 
 
 def get_element(class_tag: str) -> generator:
+    """This function gets the class label and returns the next class element"""
     path = os.path.join("dataset", class_tag)
     names_list = os.listdir(path)
     names_list.append(None)
