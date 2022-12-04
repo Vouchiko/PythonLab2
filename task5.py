e376aeb89ef44c75cb13eb4002544cb16d6f7a08
vouchiko <vovakarpov2017@gmail.com>
2022-12-04 17:21:29 +0400

    Added the second iterator class

--- task5.py
+++ task5.py
@@ -11,7 +11,25 @@ class IteratorTask1:
     def __next__(self):
         if self.counter < self.limit:
             self.counter += 1
-            return os.path.join(self.path, self.file_names[self.counter-1])
+            return os.path.join(self.path, self.file_names[self.counter - 1])
         else:
             raise StopIteration
 
+
+class IteratorTask2:
+    def __init__(self, class_name: str, path: str):
+        self.file_names = os.listdir(os.path.join(path))
+        for name in self.file_names:
+            if not class_name in name:
+                self.file_names.remove(name)
+
+        self.limit = len(self.file_names)
+        self.counter = 0
+        self.path = path
+
+    def __next__(self):
+        if self.counter < self.limit:
+            self.counter += 1
+            return os.path.join(self.path, self.file_names[self.counter - 1])
+        else:
+            raise StopIteration
