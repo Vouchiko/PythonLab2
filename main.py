830e632767c37338ea312ccc4ab8736028ee5aa1
vouchiko <vovakarpov2017@gmail.com>
2022-12-04 15:21:28 +0400

    Added task3

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from task1 import run1
 from task2 import run2
+from task3 import run3
+
+
 if __name__ == '__main__':
-    run1('brown bear', 'annotation1.csv')
+    run1('brown bear', 'annotation.csv')
     run2('Brown_bear_copy', 'annotation.csv')
+    run3('annotation.csv', 'dataset2')
---
+++ task3.py
@@ -0,0 +1,35 @@
+import random
+import os
+import shutil
+import csv
+from email import generator
+
+
+def create_dir(dir_name: str) -> str:
+    if not os.path.isdir(dir_name):
+        os.mkdir(dir_name)
+    return dir_name
+
+
+def get_element(class_name: str) -> generator:
+    for file_name in os.listdir(os.path.join("dataset", class_name)):
+        yield file_name
+
+
+def create_randname_file(annotation_name: str, dir_copy: str) -> None:
+    file_number = list(range(10001))
+    random.shuffle(file_number)
+    count = 1
+    create_dir(dir_copy)
+    for dataset_class in os.listdir("dataset"):
+        for file_name in get_element(dataset_class):
+            shutil.copy(os.path.join(os.path.join("dataset", dataset_class), file_name),
+                        os.path.join(dir_copy, f"{file_number[count]}.jpg"))
+            with open(os.path.join(dir_copy, annotation_name), mode="a", encoding="UTF-16", newline='') as file:
+                file_writer = csv.writer(file, delimiter=",")
+                file_writer.writerow([f"{file_number[count]}.jpg", dataset_class])
+            count += 1
+
+
+def run3(annotation_name: str, dir_copy: str) -> None:
+    create_randname_file(annotation_name, dir_copy)
